066af8c2c60eb32c880eb07b61c6cf410f55f612
john nicholas <themrtortoise@gmail.com>
2016-12-16 23:08:55 +0000

    http collect

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,13 +1,18 @@
 
 
 let url = process.argv[2];
-
-
-var http = require('http')
+var http = require('http');
+var bl = require('bl');
 
 http.get(url, function urlCallback(response) {
     response.setEncoding("utf8");
-    response.on("data", console.log);
-    response.on("error", console.error)
+    response.pipe(bl(function (err, data) {
+        if (err) {
+            return console.error(err)
+        }
+        var bufferString = data.toString();
+        console.log(bufferString.length);
+        console.log(bufferString);
+    }));
 }).on('error', console.error);
 
