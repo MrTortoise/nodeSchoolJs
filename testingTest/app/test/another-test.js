d6414a257218e61f02ecea865b02ade59e8f3185
john nicholas <themrtortoise@gmail.com>
2016-12-19 21:34:49 +0000

    tests using tape and tape-spec
    example tests, runs all way through, formats.
    Requires node-glob to be installed

---
+++ testingTest/app/test/another-test.js
@@ -0,0 +1,9 @@
+let test = require('tape');
+
+test('simple passing equality test', function (t) {
+    t.plan(2);
+
+    t.equal(12, 6 + 5);
+
+    t.equal('fred', 'fred');
+});
--- testingTest/app/test/someFunction-test.js
+++ testingTest/app/test/someFunction-test.js
@@ -3,7 +3,7 @@ let test = require('tape');
 test('simple passing equality test', function (t) {
     t.plan(2);
 
-    t.equal(12, 6 + 5);
+    t.equal(12, 6 + 6);
 
-    t.equal('fred', 'red');
+    t.equal('fred', 'fred');
 });
